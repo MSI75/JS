e50e3052560474c11476bf0d6ea7fabbfbc65b37
MSI75 <sisaif56@gmail.com>
2023-01-02 12:09:00 +0600

    variable.js

---
+++ chapterTwo/variable.js
@@ -0,0 +1,12 @@
+// var name = "MSI Saif";
+// var age = 23;
+
+console.log(name+" know JavaScript");
+console.log("My age is :- "+age);
+
+var name = "MSI Saif";
+var age = 23;
+
+var mathematicsNumber = 10;
+var accountNumberDetailsId = 104;
+var account_number_details_id = 2;
